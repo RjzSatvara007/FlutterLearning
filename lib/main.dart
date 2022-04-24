a592bc27c4547ca5411d931d934ee8e1daaa8549
rajesh <rajesh.sathvara007@gmail.com>
2022-04-24 18:09:35 +0530

    App-bar UI creation with click listener.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp("My App", "Rajesh Satvara"));
 }
 
-var title = "My First Demo";
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  String title = "";
+  String name = "";
+
+  MyApp(this.title, this.name);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () => print("leading clicked"),
+          ),
           title: Text(title),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add_alert),
+              tooltip: 'Alerts',
+              onPressed: () => print("Alerts clicked"),
+            ),
+            PopupMenuButton(
+                icon: const Icon(Icons.settings), // dynamic icon
+                onSelected: (newValue) => print("$newValue clicked"),
+                itemBuilder: (context) {
+                  return [
+                    const PopupMenuItem(
+                      child: Text("My Account"),
+                      value: 1,
+                    ),
+                    const PopupMenuItem(
+                      child: Text("Log-out"),
+                      value: 2,
+                    ),
+                  ];
+                })
+          ],
         ),
-        body: Container(),
-        drawer: const Drawer(),
+        body: Center(
+          child: Text("Welcome , $name"),
+        ),
       ),
     );
   }
